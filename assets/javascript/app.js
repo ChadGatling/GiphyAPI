dae0c648cb727b38ae545ba9025b524dead6e81f
Elliott Wendel <ewendel88@gmail.com>
2017-09-07 10:47:53 -0500

    working, not finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,71 @@
 $(document).ready(function() {
     var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=6d635480f9444189bb2125786f5b586b";
-    var searchList = ["Trending"];
+    var searchList = [];
 
-    $.ajax({
+    $(".gifSearchButton").click(clickGifSearchButton); // When clicked
+
+    $.ajax({ // Get the API data
         url: queryURL,
         method: 'GET'
-    }).done(function(response) {
-        console.log(response.data);
+    }).done(function(response) { // Do something with the data
+        // console.log(response.data);
 
-        for (var i = 0; i < response.data.length; i++) {
+        for (var i = 0; i < response.data.length; i++) { // Add gifs to html
             var gifURL = response.data[i].images.fixed_height_small.url;
             var gifRating = response.data[i].rating.toUpperCase();
 
-            $(".gifs").append(
+            $(".gifs").append( // Html to be appended
                 '<div class="image">' +
                 '<img src="' + gifURL + '" />' +
                 '<span class="rating">' + gifRating + '</span>' +
                 '</div>'
             );
         }
+    }); 
 
-        $(".gifSearchButton").click(clickGifSearchButton);
-    });
+    buildSearchButtons();
 
-    buildSearchButtons();       
-
-    function clickGifSearchButton(event) {
+    function clickGifSearchButton(event) { // push search term into searchList array
         event.preventDefault();
         console.log($(".gifSearch").val());
         searchList.push($(".gifSearch").val());
-        $(".searchList").empty();
-        $(".gifSearch")
+        $(".gifSearch").val("");
         buildSearchButtons();
     }
-    function buildSearchButtons() {
+    function buildSearchButtons() { // repopulate header with search term buttons
+        $(".searchList").empty();
         for (var i = 0; i < searchList.length; i++) {
             $(".searchList").append(
-            '<button>' + searchList[i] + '</button>'
+            '<button type="submit" class="termButton">' + searchList[i] + '</button>'
             );
         }
-    }    
+        $(".termButton").click(getButtonTerm);
+    }
+    function getButtonTerm(event) { // get the gifs that were searched for
+        event.preventDefault();
+        console.log($(this).text());
+
+        var searchTerm = $(this).text();
+        var searchURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=6d635480f9444189bb2125786f5b586b";
+
+        $(".image").empty();
+        $.ajax({ // Get the API data
+            url: searchURL,
+            method: 'GET'
+        }).done(function(response) { // Do something with the data
+            // console.log(response.data);
+
+            for (var i = 0; i < response.data.length; i++) { // Add gifs to html
+                var gifURL = response.data[i].images.fixed_height_small.url;
+                var gifRating = response.data[i].rating.toUpperCase();
+
+                $(".gifs").append( // Html to be appended
+                    '<div class="image">' +
+                    '<img src="' + gifURL + '" />' +
+                    '<span class="rating">' + gifRating + '</span>' +
+                    '</div>'
+                );
+            }
+        }); 
+    }
 });
